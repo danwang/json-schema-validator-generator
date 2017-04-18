1ed6cb9bd31f1c8dc902bd2a1c24fca45e4850f6
Dan Wang <github.public@danwanf.com>
2017-04-18 03:00:27 -0700

    Publish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // @flow
+import generateFlow from 'jsvg/flow/generate.js';
 import generateValidator from 'jsvg/js/generate.js';
 
-export default {
-  generateValidator,
+type Schemas = {[key: string]: Object};
+type Generated = {
+  flow: string,
+  js: string,
 };
+
+const generate = (schema: Object, shape: Schemas = {root: schema}): Generated => {
+  return {
+    flow: generateFlow(schema, shape),
+    js: generateValidator(schema, shape),
+  };
+};
+
+export default generate;
